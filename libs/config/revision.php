fdd944131f0416e7a8438e46b1228c2b41da7ed4
Andrei Neculau <andrei.neculau@gmail.com>
2011-08-06 15:10:28 +0200

    Fix LL_VERSION

--- libs/config/revision.php
+++ libs/config/revision.php
@@ -4,9 +4,9 @@ $ll_version_date = `git show -s --format="%ct" HEAD`;
 $ll_version_date = date('Y-m-d', $ll_version_date);
 $ll_version_hash = `git show -s --format="%h" HEAD`;
 
-define('LL_VERSION_MAJOR', $ll_version_date);
-define('LL_VERSION_MINOR', $ll_version_hash);
 define('LL_VERSION_STATUS', 'beta');
+define('LL_VERSION', $ll_version_date);
+define('LL_VERSION_MINOR', $ll_version_hash);
 
 unset($ll_version_date);
 unset($ll_version_hash);
